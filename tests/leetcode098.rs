be986609c39db4e5e4b4c06530258d9d9d4c3fc5
KODAMA Yoichi <kodama.yoichi@gmail.com>
2023-02-25 16:26:36 +0900

    add 098

--- tests/leetcode098.rs
+++ tests/leetcode098.rs
@@ -22,11 +22,24 @@ pub struct Solution {}
 use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
-    fn recursive(node: Option<Rc<RefCell<TreeNode>>>) -> (i32, i32, bool) {
-        (0, 0, true)
+    fn recursive(node: Option<Rc<RefCell<TreeNode>>>, min: i64, max: i64) -> bool {
+        match node {
+            None => true,
+            Some(node) => {
+                let val = node.borrow().val as i64;
+                if val <= min || max <= val {
+                    return false;
+                }
+                let answer = Self::recursive(node.borrow_mut().left.take(), min, val);
+                if !answer {
+                    return false;
+                }
+                Self::recursive(node.borrow_mut().right.take(), val, max)
+            }
+        }
     }
     pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        true
+        Self::recursive(root, -2147483649, 2147483648)
     }
 }
 
@@ -78,155 +91,70 @@ mod leetcode098 {
         assert_eq!(false, Solution::is_valid_bst(node5));
     }
 
-    // #[test]
-    // fn test5() {
-    //     // [4,2,7,1,3]
-    //     let node1 = Some(Rc::new(RefCell::new(TreeNode {
-    //         val: 1,
-    //         left: None,
-    //         right: None,
-    //     })));
-    //     let node3 = Some(Rc::new(RefCell::new(TreeNode {
-    //         val: 3,
-    //         left: None,
-    //         right: None,
-    //     })));
-    //     let node7 = Some(Rc::new(RefCell::new(TreeNode {
-    //         val: 7,
-    //         left: None,
-    //         right: None,
-    //     })));
-    //     let node2 = Some(Rc::new(RefCell::new(TreeNode {
-    //         val: 2,
-    //         left: node1,
-    //         right: node3,
-    //     })));
-    //     let node4 = Some(Rc::new(RefCell::new(TreeNode {
-    //         val: 4,
-    //         left: node2,
-    //         right: node7,
-    //     })));
+    #[test]
+    fn test5() {
+        // [4,2,7,1,3]
+        let node1 = Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: None,
+        })));
+        let node3 = Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: None,
+            right: None,
+        })));
+        let node5 = Some(Rc::new(RefCell::new(TreeNode {
+            val: 5,
+            left: None,
+            right: None,
+        })));
+        let node7 = Some(Rc::new(RefCell::new(TreeNode {
+            val: 7,
+            left: node5,
+            right: None,
+        })));
+        let node2 = Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: node1,
+            right: node3,
+        })));
+        let node4 = Some(Rc::new(RefCell::new(TreeNode {
+            val: 4,
+            left: node2,
+            right: node7,
+        })));
 
-    //     // [4,2,7,1,3,5]
-    //     let node1b = Some(Rc::new(RefCell::new(TreeNode {
-    //         val: 1,
-    //         left: None,
-    //         right: None,
-    //     })));
-    //     let node3b = Some(Rc::new(RefCell::new(TreeNode {
-    //         val: 3,
-    //         left: None,
-    //         right: None,
-    //     })));
-    //     let node5b = Some(Rc::new(RefCell::new(TreeNode {
-    //         val: 5,
-    //         left: None,
-    //         right: None,
-    //     })));
-    //     let node7b = Some(Rc::new(RefCell::new(TreeNode {
-    //         val: 7,
-    //         left: node5b,
-    //         right: None,
-    //     })));
-    //     let node2b = Some(Rc::new(RefCell::new(TreeNode {
-    //         val: 2,
-    //         left: node1b,
-    //         right: node3b,
-    //     })));
-    //     let node4b = Some(Rc::new(RefCell::new(TreeNode {
-    //         val: 4,
-    //         left: node2b,
-    //         right: node7b,
-    //     })));
-    //     assert_eq!(node4b, Solution::insert_into_bst(node4, 5));
-    // }
+        assert_eq!(true, Solution::is_valid_bst(node4));
+    }
 
-    // #[test]
-    // fn test7a() {
-    //     let node40;
-    //     {
-    //         // [40,20,60,10,30,50,70]
-    //         let node10 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 10,
-    //             left: None,
-    //             right: None,
-    //         })));
-    //         let node30 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 30,
-    //             left: None,
-    //             right: None,
-    //         })));
-    //         let node20 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 20,
-    //             left: node10,
-    //             right: node30,
-    //         })));
-    //         let node50 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 50,
-    //             left: None,
-    //             right: None,
-    //         })));
-    //         let node70 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 70,
-    //             left: None,
-    //             right: None,
-    //         })));
-    //         let node60 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 60,
-    //             left: node50,
-    //             right: node70,
-    //         })));
-    //         node40 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 40,
-    //             left: node20,
-    //             right: node60,
-    //         })));
-    //     }
-
-    //     let node40b;
-    //     {
-    //         // [40,20,60,10,30,50,70,null,null,25]
-    //         let node25 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 25,
-    //             left: None,
-    //             right: None,
-    //         })));
-    //         let node10 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 10,
-    //             left: None,
-    //             right: None,
-    //         })));
-    //         let node30 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 30,
-    //             left: node25,
-    //             right: None,
-    //         })));
-    //         let node20 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 20,
-    //             left: node10,
-    //             right: node30,
-    //         })));
-    //         let node50 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 50,
-    //             left: None,
-    //             right: None,
-    //         })));
-    //         let node70 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 70,
-    //             left: None,
-    //             right: None,
-    //         })));
-    //         let node60 = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 60,
-    //             left: node50,
-    //             right: node70,
-    //         })));
-    //         node40b = Some(Rc::new(RefCell::new(TreeNode {
-    //             val: 40,
-    //             left: node20,
-    //             right: node60,
-    //         })));
-    //     }
-    //     assert_eq!(node40b, Solution::insert_into_bst(node40, 25));
-    // }
+    #[test]
+    fn test222() {
+        // [2,2,2]
+        let node2a = Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: None,
+            right: None,
+        })));
+        let node2b = Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: None,
+            right: None,
+        })));
+        let node2 = Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: node2a,
+            right: node2b,
+        })));
+        assert_eq!(false, Solution::is_valid_bst(node2));
+    }
+    #[test]
+    fn test2147483647() {
+        let node2147483647 = Some(Rc::new(RefCell::new(TreeNode {
+            val: 2147483647,
+            left: None,
+            right: None,
+        })));
+        assert_eq!(true, Solution::is_valid_bst(node2147483647));
+    }
 }
